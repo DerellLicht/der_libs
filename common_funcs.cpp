800300c9ff08f9099e44cf5c61f04fd98a09d955
Derell Licht <derelict@comcast.net>
2025-03-05 14:29:29 -0800

    switching to new compiler

--- common_funcs.cpp
+++ common_funcs.cpp
@@ -1050,7 +1050,7 @@ TCHAR *convert_to_commas(ULONGLONG uli, TCHAR *outstr)
 
    //sprintf(temp_ull_str, "%llu", uli);
    //_stprintf(temp_ull_str, _T( "%"PRIu64""), uli);
-   _stprintf(temp_ull_str, "%I64u", uli);
+   _stprintf(temp_ull_str, _T("%I64u"), uli);
    // _ui64toa(uli, temp_ull_str, 10) ;
    slen = _tcslen(temp_ull_str) ;
    inIdx = --slen ;//  convert byte-count to string index 
