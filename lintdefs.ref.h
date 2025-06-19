2b61ad4d9022ce51392e52f39601f183481b31a5
Derell Licht <derelict@comcast.net>
2025-06-18 17:06:29 -0700

    merge reference lint exclusions file

--- lintdefs.ref.h
+++ lintdefs.ref.h
@@ -1,5 +1,8 @@
 //  PcLint warnings/errors caused by PcLint not supporting STL, C++11 standard, TCHAR
 
+//  this is a long-unexplained message
+//lint -e10    Expecting ';'
+
 //lint -esym(14,  __created)   Symbol not referenced
 //lint -esym(401, __created)   Symbol not referenced
 //lint -esym(528, __created)   Symbol not referenced
